643ab0c7a227275eceed93e9cee971da75c366fa
mechztcc <dojathan@gmail.com>
2022-05-12 10:01:01 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_food_owner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_food_owner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_owner/app/modules/home/home_store.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({Key? key, this.title = "Home"}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends ModularState<HomePage, HomeStore> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: Center(
+        child: Observer(
+          builder: (context) => Text('${store.counter}'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          store.increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
